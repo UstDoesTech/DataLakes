37d296a33db8869417bfa082016981a24d92de26
Ust Oldfield <uoldfield@users.noreply.github.com>
2018-04-24 17:41:37 +0100

    Create DeployADLS.ps1

---
+++ Framework/ADLS Management/Create ADLS/DeployADLS.ps1
@@ -0,0 +1,1 @@
+.\ADLS.Deploy.Ps1 -subscriptionId "00000000-0000-0000-0000-000000000000" -resourceGroupName "adventureworksrgweu" -resourceGroupLocation "West Europe" -deploymentName "AzureDataLakeStore" -templateFilePath "ADLS_Template.json" -parametersFilePath "ADLS_Parameters.json"
