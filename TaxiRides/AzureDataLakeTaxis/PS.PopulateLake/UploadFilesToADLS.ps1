095b622a9df8fec97834278f38032fbbdbf5dd63
Ust Oldfield <uoldfield@users.noreply.github.com>
2018-04-25 10:58:10 +0100

    Update UploadFilesToADLS.ps1

--- TaxiRides/AzureDataLakeTaxis/PS.PopulateLake/UploadFilesToADLS.ps1
+++ TaxiRides/AzureDataLakeTaxis/PS.PopulateLake/UploadFilesToADLS.ps1
@@ -3,10 +3,10 @@
 #
 
 Login-AzureRmAccount
-Set-AzureRmContext -SubscriptionId 0ab62d65-05dc-4f87-aa35-f3b460bbc42b
+Set-AzureRmContext -SubscriptionId 000000-0000-0000-0000000000
 
-$myrootdir ="/Raw/Taxis/2013"
-$dataLakeStoreName = "sqlbits2017adls"
+$myrootdir ="/RAW/Taxis/2013"
+$dataLakeStoreName = "datalakestore"
 
 Import-AzureRmDataLakeStoreItem -AccountName $dataLakeStoreName -Path "F:\Taxis\2013" -Destination $myrootdir
 
